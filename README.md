4b5dd152bdc306d962e544a2f45cd0dbcc91cc40
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-10-19 17:29:00 +0530

    completed package

---
+++ README.md
@@ -0,0 +1,28 @@
+# Image-to-String
+
+A Simple image to base64 string converter application.
+
+### How to get started ?
+
+1. CLick on Green Code button and download the project
+
+2. Open terminal in project's root directory
+
+3. Execute the below command to install all dependencies
+```bash
+npm install 
+```
+
+4. Execute the below command to start the project server
+```bash
+npm start
+```
+
+5. Optionally you can execute the bellow command to get a production build 
+```bash
+npm build
+```
+
+## Demo
+
+![](/public/img.png)
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useRef, useState } from "react"
 
 const App = () => {
 
-
     const [ image, setImage ] = useState()
     const [ str, setStr ] = useState()
     const copytext = useRef()
@@ -16,24 +15,34 @@ const App = () => {
     const copy = () => {
         copytext.current.select()
         document.execCommand("copy")
-        alert("string copied")
     }
 
     return (
-        <div>
+        <div className="flex items-center justify-center flex-col min-h-[100vh]">
+
+            <h1 className="font-medium italic text-lg m-4">Image To String Converter</h1>
+
             <div>
                 <input 
+                    className="border rounded border-gray-300 p-2 hover:cursor-pointer"
                     type="file"  
                     accept="image/*"
                     onChange={(e)=>setImage(e.target.files[0])}
                 />
             </div>
+
             <div>
-                <button onClick={imageToString}>Convert</button>
+                <button onClick={imageToString} className="hover:bg-red-500 bg-red-600 text-white rounded p-2 mt-4">
+                    Convert
+                </button>
             </div>
-            <textarea ref={copytext} value={str} id="area" cols="70" rows="10">
-            </textarea>
-            <button  onClick={copy}>Copy</button>
+
+            <textarea className="border-gray-300 border m-2 focus:!outline-none" ref={copytext} value={str} id="area" cols="25" rows="6"></textarea>
+
+            <div>
+                <button className="bg-blue-700 hover:bg-blue-600 text-white px-3 py-2 rounded" onClick={copy}>Copy Text</button>
+            </div>
+
         </div>
     )
 }
